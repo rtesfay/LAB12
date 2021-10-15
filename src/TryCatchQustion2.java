5607b1f8616ac3c4afa32463228f08a9a0f3a929
rteklu <rahelteklu@gmail.com>
2021-10-15 02:30:04 -0500

    Initial commit

---
+++ src/TryCatchQustion2.java
@@ -0,0 +1,24 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TryCatchQustion2 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        try {
+            int num=Scanner.nextInt();
+            if (num!=0){
+                throw new Exception("Not Zero!");
+            }
+            System.out.println("I am happy with the input"); //when the input is 0
+        }catch (InputMismatchException e){
+
+            System.out.println("Invalid Entry"); //when the input is 12xy
+
+        }catch (Exception e){
+        System.out.println("Error: "+e.getMessage()); //when the input is -1
+    }
+        finally {
+            System.out.println("Finally clause Executed");// Finally clause Executed
+    }
+}
+}
